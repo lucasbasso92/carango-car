15169e2149f911c45370d9c6aebbd439b3815ae0
Lucas Basso1~ <lucasbasso92@hotmail.com>
2025-05-13 19:33:33 -0300

    corrigindo erro de sintaxe no main.js

--- main.js
+++ main.js
@@ -4,24 +4,17 @@ const { app, BrowserWindow } = require('electron')
 // Define a funcao createwindow que é responsavel e cria uma janela e aplicativo electron
 
 const createWindow = () => {
-    // Cria uma nova instancia do BrowserWindow com as dimensoes  especificas
-  
-    const win = new BrowserWindow({
+  // Cria uma nova instancia do BrowserWindow com as dimensoes especificas
+  const win = new BrowserWindow({
     width: 800, // define a largura da janela
-    height: 600   // define a altura da janela
-  })
+    height: 600  // define a altura da janela
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
-
-//Quando o aplicativo estiver pronto, chama a função createwindow para criar a janela principal 
-
-
+// Quando o aplicativo estiver pronto, chama a função createwindow para criar a janela principal
 app.whenReady().then(() => {
-//Executa  a função createwindow para criar a janela principal
-
-
-
-  createWindow()
-})
+  // Executa a função createwindow para criar a janela principal
+  createWindow();
+});
